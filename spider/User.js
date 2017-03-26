f1732026279f4318e1a3c2110af91c9996cfc889
regaliastar <1183080130@qq.com>
2017-03-26 14:12:33 +0800

    user

---
+++ spider/User.js
@@ -0,0 +1,22 @@
+/**
+ *create by regaliastar on 3/26
+ *
+ *用于得到P站所有用户的信息，并保存在文件或数据库中
+ */
+ var fetch = require('./fetch'),
+ 	fs = require('fs'),
+ 	cheerio = require('cheerio'),
+ 	request = require('request'),
+ 	log = require('./../log'),
+	createHeader = require('./requestHeader');
+
+var getUser = (function(){
+	var seed = 'http://www.pixiv.net/member.php?id=';
+
+	return function(ID){
+		var url = seed+ID;
+		fetch(url,function($){
+			
+		});
+	}
+})();
