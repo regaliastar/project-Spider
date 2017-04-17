67cac2dc4b7d0e7ae78d219a3ccd52e18c260631
regaliastar <1183080130@qq.com>
2017-04-17 23:59:02 +0800

    重写了Log类

--- log.js
+++ log.js
@@ -1,26 +1,87 @@
+'use strict';
+
 /**
  * @author regaliastar
  *
- * 传入任意个字符串，将其按空格分开，并按时间作为文件名写在调用路径的logs目录下
+ * Log类，用于打印日志以调试错误信息
+ *
  */
 var fs = require('fs');
 
-function log(){
-	var d = new Date();
-	var date = d.getFullYear()+"-"+(d.getMonth()+1)+"-"+d.getDate();
-	var time = '['+d.getHours()+'时'+d.getMinutes()+'分'+d.getSeconds()+'秒] ';
+class Log{
+	/**
+	 * @param re	支持自定义前缀
+	 */
+	constructor(re){
+		if(re)	re = '['+re+']';
+		this.re = re || '';
+	}
+
+}
+
+/**
+ * 写文件
+ */
+Log.log = function(){
+	var date = Log.getDate();
+	var time = Log.getTime();
 
 	if(!fs.existsSync('./logs/')){
 		fs.mkdirSync('./logs/');
 	}
 
-	var txt = [].join.call(arguments,' ');
+	var args = Array.prototype.slice.call(arguments);
+	var txt = [].join.call(args,' ');
 
-	fs.appendFile('./logs/'+date+'.log','\n'+time+txt,'utf-8',function(err){
+	fs.appendFile('./logs/'+date+'.log',time+txt+'\n','utf-8',function(err){
 		if(err){
 			throw err;
 		}
 	})
 }
 
-module.exports = log;
+/**
+ * 用于记录一般信息，INFO级别，主要为通知信息
+ * 传入任意个字符串，将其按空格分开，并按时间作为文件名写在调用路径的logs目录下
+ *
+ * @param this.re 在传入前缀re的情况下，info前缀会改变成设定的值
+ * @see Log.log()
+ */
+Log.prototype.info = function(){
+	this.re = this.re || '[INFO]';
+	var args = Array.prototype.slice.call(arguments);
+	args.unshift(this.re);
+	this.re = '';
+	Log.log.apply(this,args);
+}
+
+/**
+ * 用于记录错误信息，ERROR级别
+ *
+ * @see Log.log()
+ */
+Log.prototype.error = function(){
+	var args = Array.prototype.slice.call(arguments);
+	args.unshift('[ERROR]');
+	Log.log.apply(this,args);
+}
+
+/**
+ * 返回如YYYY-MM-DD格式的日期
+ */
+Log.getDate = function(){
+	var d = new Date();
+	var date = d.getFullYear()+"-"+(d.getMonth()+1)+"-"+d.getDate();
+	return date;
+}
+
+/**
+ * 返回如[HH时MM分SS秒]格式的时间
+ */
+Log.getTime = function(){
+	var d = new Date();
+	var time = '['+d.getHours()+'时'+d.getMinutes()+'分'+d.getSeconds()+'秒] ';
+	return time;
+}
+
+module.exports = Log;
--- spider/test.js
+++ spider/test.js
@@ -6,6 +6,8 @@
 HTMLParser.parseMutilWork('http://www.pixiv.net/member_illust.php?mode=medium&illust_id=60909471',function(w){
     w.print();
 })*/
+
+/*
 var Filter = require('./class/Filter');
 var filter = new Filter({'praise':0,"praise_pro":0,"pageView":0,"has_tag_every":[],"has_tag_some":['2'],"no_tag_any":['1'],"no_tag_every":['1','10']});
 var workList = [{'praise':10,"praise_pro":0,"pageView":100,"tags":['1','10'],"url":'www',"pixiver":'118'},
@@ -13,4 +15,10 @@ var workList = [{'praise':10,"praise_pro":0,"pageView":100,"tags":['1','10'],"ur
                 {'praise':20,"praise_pro":0,"pageView":200,"tags":['2'],"url":'www',"pixiver":'118'},
                 {'praise':30,"praise_pro":0,"pageView":300,"tags":['3'],"url":'www',"pixiver":'118'}];
 var resultSet = filter.filter(workList);
-console.log('resultSet: '+JSON.stringify(resultSet));
+console.log('resultSet: '+JSON.stringify(resultSet));*/
+
+
+var Log = require('./../Log');
+var log = new Log('555');
+log.info('hhh');
+log.error('error!!');
